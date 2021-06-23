16afb8397ed4478f755ea0f9bdebe8051386d239
Dzole <dzolekirovski@gmail.com>
2021-06-23 20:59:51 +0200

    finished js and edited html

---
+++ dom.js
@@ -0,0 +1,14 @@
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+
+// Form submit event
+
+form.addEventListener('submit', addItem);
+
+// Add Item;
+
+function addItem(e){
+    e.preventDefault;
+
+    console.log(1);
+}
